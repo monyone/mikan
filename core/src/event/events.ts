8e8d8dc070ecffe0c3de07703053a7a02a52566d
monyone <monyone.teihen@gmail.com>
2022-10-19 02:27:22 +0900

    add event for AUTH and TERMINATED

--- core/src/event/events.ts
+++ core/src/event/events.ts
@@ -2,6 +2,12 @@ export const EventTypes = {
   RTMP_CHUNK_RECIEVED: 'RTMP_CHUNK_RECIEVED',
   RTMP_CHUNK_SEND: 'RTMP_CHUNK_SEND',
 
+  RTMP_APP_INVALID: 'RTMP_STREAMKEY_INVALID',
+  RTMP_STREAMKEY_INVALID: 'RTMP_STREAMKEY_INVALID',
+
+  RTMP_PUBLISHED: 'RTMP_PUBLISHED',
+  RTMP_TERMINATED: 'RTMP_TERMINATED',
+
   FLV_CHUNK_OUTPUT: 'FLV_CHUNK_OUTPUT' 
 } as const;
 
@@ -15,6 +21,32 @@ export type RTMP_CHUNK_SEND_PAYLOAD = {
   chunk: ArrayBuffer
 }
 
+export type RTMP_APP_INVALID_PAYLOAD = {
+  event: typeof EventTypes.RTMP_APP_INVALID;
+  required: string,
+  actual: string
+}
+
+export type RTMP_STREAMKEY_INVALID_PAYLOAD = {
+  event: typeof EventTypes.RTMP_STREAMKEY_INVALID;
+  required: string,
+  actual: string
+}
+
+export type RTMP_PUBLISHED_PAYLOAD = {
+  event: typeof EventTypes.RTMP_PUBLISHED,
+  app: string,
+  streamKey: string,
+  messageId: number
+}
+
+export type RTMP_TERMINATED_PAYLOAD = {
+  event: typeof EventTypes.RTMP_TERMINATED,
+  app: string,
+  streamKey: string,
+  messageId: number
+}
+
 export type FLV_CHUNK_OUTPUT_PAYLOAD = {
   event: typeof EventTypes.FLV_CHUNK_OUTPUT,
   chunk: ArrayBuffer
@@ -23,6 +55,12 @@ export type FLV_CHUNK_OUTPUT_PAYLOAD = {
 export type Events = {
   [EventTypes.RTMP_CHUNK_RECIEVED]: RTMP_CHUNK_RECIEVED_PAYLOAD,
   [EventTypes.RTMP_CHUNK_SEND]: RTMP_CHUNK_SEND_PAYLOAD
+
+  [EventTypes.RTMP_APP_INVALID]: RTMP_APP_INVALID_PAYLOAD,
+  [EventTypes.RTMP_STREAMKEY_INVALID]: RTMP_STREAMKEY_INVALID_PAYLOAD,
+  
+  [EventTypes.RTMP_PUBLISHED]: RTMP_PUBLISHED_PAYLOAD,
+  [EventTypes.RTMP_TERMINATED]: RTMP_TERMINATED_PAYLOAD,
 
   [EventTypes.FLV_CHUNK_OUTPUT]: FLV_CHUNK_OUTPUT_PAYLOAD
 }
--- core/src/reader/index.ts
+++ core/src/reader/index.ts
@@ -14,8 +14,18 @@ enum HandshakeState {
   ESTABLISHED
 }
 
+enum ConnectionState {
+  WAITING_CONNECT,
+  WAITING_FCPUBLISH,
+  WAITING_PUBLISH,
+  PUBLISHED,
+  TERMINATED
+}
+
 type ReaderOption = {
   dumpFLV: boolean
+  app?: string,
+  streamKey?: string
 };
 
 export default class Reader {
@@ -23,12 +33,14 @@ export default class Reader {
   #option: ReaderOption;
 
   #handshakeState: HandshakeState = HandshakeState.WAITING_ZERO;
+  #connectionState: ConnectionState = ConnectionState.WAITING_CONNECT;
   #time: number = 0;
   #ownRandom: ArrayBuffer = bulidRandom(1528);
 
   #chunkReciever = new ChunkReciever();
   #chunkSize: number = 128;
-  #streamName = '';
+  #app = '';
+  #streamKey = '';
 
   // for FLV
   #priviousTagSize: number = 0;
@@ -57,6 +69,8 @@ export default class Reader {
 
   #onRtmpChunkRecieved({ chunk }: Events[typeof EventTypes.RTMP_CHUNK_RECIEVED]) {
     let begin = 0;
+
+    // if Handshaking
     switch (this.#handshakeState) {
       case HandshakeState.WAITING_ZERO: {
         if (chunk.byteLength < begin + 1) { break; }
@@ -121,122 +135,203 @@ export default class Reader {
         chunk = chunk.slice(begin);
       }
       case HandshakeState.ESTABLISHED: {
-        for (const info of this.#chunkReciever.recieveChunk(chunk, this.#chunkSize)) {
-          if (this.#option.dumpFLV) {
-            if (this.#flvNeedsHeader) {
-              this.#emitter.emit(EventTypes.FLV_CHUNK_OUTPUT, {
-                event: EventTypes.FLV_CHUNK_OUTPUT,
-                chunk: Uint8Array.from([
-                  0x46, 0x4C, 0x56, 1, 4 | 1, 0, 0, 0, 9
-                ]).buffer
-              });
-              this.#flvNeedsHeader = false;
-            }
-
-            info.timestamp = Math.max(info.timestamp, this.#privousDTS);
-            const toFLV = flv(info, this.#priviousTagSize);
-
-            toFLV.forEach((ch) => {
-              this.#priviousTagSize = ch.byteLength - 4;
-              this.#emitter.emit(EventTypes.FLV_CHUNK_OUTPUT, {
-                event: EventTypes.FLV_CHUNK_OUTPUT,
-                chunk: ch
-              });
-            });
-            this.#privousDTS = info.timestamp + toFLV.length;
-          }
-          
-          const message = concat(... info.message);
-          if (info.message_type_id === 1) {
-            // for chunk size
-            const view = new DataView(message);
-            this.#chunkSize = view.getUint32(0, false);
-          } else if (info.message_type_id === 20) { // AMF0
-            const amf = parseAMF(message);
-            
-            if (!Array.isArray(amf)) { continue; }
-            const [name, transaction_id, ... objs] = amf;
-
-            switch(name) {
-              case 'connect': {
-                // ack
-                for (const chunk of generateWindowAcknowledgementSize(this.#time, 2500000)) {
-                  this.#emitter.emit(EventTypes.RTMP_CHUNK_SEND, {
-                    event: EventTypes.RTMP_CHUNK_SEND,
-                    chunk
-                  });
-                }
-
-                // peer
-                for (const chunk of generateSetPeerBandwidth(this.#time, 2500000, 2)) {
-                  this.#emitter.emit(EventTypes.RTMP_CHUNK_SEND, {
-                    event: EventTypes.RTMP_CHUNK_SEND,
-                    chunk
-                  });
-                }
-
-                // user stream
-                for (const chunk of generateUserStreamBegin(this.#time, 0)) {
-                  this.#emitter.emit(EventTypes.RTMP_CHUNK_SEND, {
-                    event: EventTypes.RTMP_CHUNK_SEND,
-                    chunk
-                  });
-                }
-
-                // chunk size
-                for (const chunk of generateSetChunkSize(this.#time, 4500)) {
-                  this.#emitter.emit(EventTypes.RTMP_CHUNK_SEND, {
-                    event: EventTypes.RTMP_CHUNK_SEND,
-                    chunk
-                  });
-                }
-                
-                // result
-                for (const chunk of generateConnectResult(this.#time, transaction_id)) {
-                  this.#emitter.emit(EventTypes.RTMP_CHUNK_SEND, {
-                    event: EventTypes.RTMP_CHUNK_SEND,
-                    chunk
-                  });
-                }
-
-                break;
-              }
-              case 'releaseStream': {
-                break;
-              }
-              case 'FCPublish': {
-                this.#streamName = objs[1];
-                for (const chunk of generateOnFCPublish(this.#time, transaction_id, this.#streamName, 1)) {
-                  this.#emitter.emit(EventTypes.RTMP_CHUNK_SEND, {
-                    event: EventTypes.RTMP_CHUNK_SEND,
-                    chunk
-                  });
-                }
-                break;
-              }
-              case 'createStream': {
-                for (const chunk of generateCreateStreamResult(this.#time, transaction_id, 1)) {
-                  this.#emitter.emit(EventTypes.RTMP_CHUNK_SEND, {
-                    event: EventTypes.RTMP_CHUNK_SEND,
-                    chunk
-                  });
-                }
-                break;
-              }
-              case 'publish': {
-                for (const chunk of generateOnStatusPublish(this.#time, transaction_id, this.#streamName)) {
-                  this.#emitter.emit(EventTypes.RTMP_CHUNK_SEND, {
-                    event: EventTypes.RTMP_CHUNK_SEND,
-                    chunk
-                  });
-                }
-                break;
-              }
-            }
-          }
-        }
         break;
       }
     }
+
+    // if Established
+    if (this.#handshakeState !== HandshakeState.ESTABLISHED) { return; }
+
+    for (const info of this.#chunkReciever.recieveChunk(chunk, this.#chunkSize)) {
+      const message = concat(... info.message);
+
+      if (info.message_stream_id === 0) { // system message
+        if (info.message_type_id === 1) {
+          // for chunk size
+          const view = new DataView(message);
+          this.#chunkSize = view.getUint32(0, false);
+        } else if (info.message_type_id === 20) { // AMF0
+          const amf = parseAMF(message);
+          
+          if (!Array.isArray(amf)) { continue; }
+          const [name, transaction_id, ... objs] = amf;
+
+          switch(name) {
+            case 'connect': {
+              if (this.#connectionState !== ConnectionState.WAITING_CONNECT) { return; }
+
+              const { app } = objs[0];
+              if (this.#option.app != null && this.#option.app !== app) {
+                // Not Valid App, ignore serving
+                this.#connectionState = ConnectionState.WAITING_CONNECT;
+                this.#handshakeState = HandshakeState.WAITING_ZERO;
+                this.#emitter.emit(EventTypes.RTMP_APP_INVALID, {
+                  event: EventTypes.RTMP_APP_INVALID,
+                  required: this.#option.app,
+                  actual: app
+                });
+                
+                return;
+              }
+              this.#app = app;
+
+              // ack
+              for (const chunk of generateWindowAcknowledgementSize(this.#time, 2500000)) {
+                this.#emitter.emit(EventTypes.RTMP_CHUNK_SEND, {
+                  event: EventTypes.RTMP_CHUNK_SEND,
+                  chunk
+                });
+              }
+
+              // peer
+              for (const chunk of generateSetPeerBandwidth(this.#time, 2500000, 2)) {
+                this.#emitter.emit(EventTypes.RTMP_CHUNK_SEND, {
+                  event: EventTypes.RTMP_CHUNK_SEND,
+                  chunk
+                });
+              }
+
+              // user stream
+              for (const chunk of generateUserStreamBegin(this.#time, 0)) {
+                this.#emitter.emit(EventTypes.RTMP_CHUNK_SEND, {
+                  event: EventTypes.RTMP_CHUNK_SEND,
+                  chunk
+                });
+              }
+
+              // chunk size
+              for (const chunk of generateSetChunkSize(this.#time, 4500)) {
+                this.#emitter.emit(EventTypes.RTMP_CHUNK_SEND, {
+                  event: EventTypes.RTMP_CHUNK_SEND,
+                  chunk
+                });
+              }
+              
+              // result
+              for (const chunk of generateConnectResult(this.#time, transaction_id)) {
+                this.#emitter.emit(EventTypes.RTMP_CHUNK_SEND, {
+                  event: EventTypes.RTMP_CHUNK_SEND,
+                  chunk
+                });
+              }
+
+              this.#connectionState = ConnectionState.WAITING_FCPUBLISH;
+              break;
+            }
+            case 'releaseStream': {
+              // FIXME: MULTI MESSAGE STREAMING (Message Stream Released)
+              break;
+            }
+            case 'FCPublish': {
+              if (this.#connectionState !== ConnectionState.WAITING_FCPUBLISH) { return; }
+
+              const streamKey = objs[1];
+              if (this.#option.streamKey != null && this.#option.streamKey !== streamKey) {
+                // Not Valid StreamKey, ignore serving
+                this.#connectionState = ConnectionState.WAITING_CONNECT;
+                this.#handshakeState = HandshakeState.WAITING_ZERO;
+                this.#emitter.emit(EventTypes.RTMP_STREAMKEY_INVALID, {
+                  event: EventTypes.RTMP_STREAMKEY_INVALID,
+                  required: this.#option.streamKey,
+                  actual: streamKey
+                });
+                return;
+              }
+
+              this.#streamKey = streamKey;
+              for (const chunk of generateOnFCPublish(this.#time, transaction_id, this.#streamKey, 1)) {
+                this.#emitter.emit(EventTypes.RTMP_CHUNK_SEND, {
+                  event: EventTypes.RTMP_CHUNK_SEND,
+                  chunk
+                });
+              }
+
+              this.#connectionState = ConnectionState.WAITING_PUBLISH;
+              break;
+            }
+            case 'createStream': {
+              // FIXME: MULTI MESSAGE STREAMING (Message Stream Created)
+
+              for (const chunk of generateCreateStreamResult(this.#time, transaction_id, 1)) {
+                this.#emitter.emit(EventTypes.RTMP_CHUNK_SEND, {
+                  event: EventTypes.RTMP_CHUNK_SEND,
+                  chunk
+                });
+              }
+              break;
+            }
+            case 'deleteStream': {
+              // FIXME: MULTI MESSAGE STREAMING (Message Stream Deleted)
+              break;
+            }
+            case 'FCUnpublish': {
+              this.#connectionState = ConnectionState.TERMINATED;
+              this.#emitter.emit(EventTypes.RTMP_TERMINATED, {
+                event: EventTypes.RTMP_TERMINATED,
+                app: this.#app,
+                streamKey: this.#streamKey,
+                messageId: 1,
+              });
+              break;
+            }
+          }
+        }
+      } else if (this.#connectionState === ConnectionState.WAITING_PUBLISH) {
+        if (info.message_type_id === 20) { // AMF0
+          const amf = parseAMF(message);
+
+          if (!Array.isArray(amf)) { continue; }
+          const [name, transaction_id, ... objs] = amf;
+          
+          switch(name) {
+            case 'publish': {
+              if (this.#connectionState !== ConnectionState.WAITING_PUBLISH) { return; }
+
+              for (const chunk of generateOnStatusPublish(this.#time, transaction_id, this.#streamKey)) {
+                this.#emitter.emit(EventTypes.RTMP_CHUNK_SEND, {
+                  event: EventTypes.RTMP_CHUNK_SEND,
+                  chunk
+                });
+              }
+
+              this.#emitter.emit(EventTypes.RTMP_PUBLISHED, {
+                event: EventTypes.RTMP_PUBLISHED,
+                app: this.#app,
+                streamKey: this.#streamKey,
+                messageId: info.message_stream_id,
+              });
+              
+              this.#connectionState = ConnectionState.PUBLISHED;
+              break;
+            }
+          }
+        }
+      } else if (this.#connectionState === ConnectionState.PUBLISHED) {
+        // FIXME: MULTI MESSAGE STREAMING (Message Stream Recieving)
+        if (!this.#option.dumpFLV) { return; }
+        
+        if (this.#flvNeedsHeader) {
+          this.#emitter.emit(EventTypes.FLV_CHUNK_OUTPUT, {
+            event: EventTypes.FLV_CHUNK_OUTPUT,
+            chunk: Uint8Array.from([
+              0x46, 0x4C, 0x56, 1, 4 | 1, 0, 0, 0, 9
+            ]).buffer
+          });
+          this.#flvNeedsHeader = false;
+        }
+  
+        info.timestamp = Math.max(info.timestamp, this.#privousDTS);
+        const toFLV = flv(info, this.#priviousTagSize);
+
+        toFLV.forEach((ch) => {
+          this.#priviousTagSize = ch.byteLength - 4;
+          this.#emitter.emit(EventTypes.FLV_CHUNK_OUTPUT, {
+            event: EventTypes.FLV_CHUNK_OUTPUT,
+            chunk: ch
+          });
+        });
+        this.#privousDTS = info.timestamp + toFLV.length;
+      }
+    }
   }
-};
+}
